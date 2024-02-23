d16a6f7d183ff1087d2e0e9e0c38b9855a0ece2f
Emanuele Maria <emanuuuelem@gmail.com>
2024-02-23 14:13:41 -0300

    feat: create models and controllers

---
+++ app/Http/Controllers/DevedorController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DevedorController extends Controller
+{
+    public function Home(){
+        echo "oi emanuele";
+    }
+}
